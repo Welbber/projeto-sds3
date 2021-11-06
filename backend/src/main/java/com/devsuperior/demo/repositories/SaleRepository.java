8bbbd6f3c5a27bf71698fde2aaeee8cbe35a11f1
Welbber Vital <welbervital01@gmail.com>
2021-11-06 13:09:57 -0300

    montando fluxo para buscar total sucesso e soma de vendas

--- backend/src/main/java/com/devsuperior/demo/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/demo/repositories/SaleRepository.java
@@ -1,9 +1,21 @@
 package com.devsuperior.demo.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.devsuperior.demo.dto.SaleSuccessDTO;
+import com.devsuperior.demo.dto.SaleSumDTO;
 import com.devsuperior.demo.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
+	@Query("SELECT NEW com.devsuperior.demo.dto.SaleSumDTO(obj.seller, SUM(obj.amount)) "
+			+ "FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSumDTO> amountGroupedBySeller();
+	
+	@Query("SELECT NEW com.devsuperior.demo.dto.SaleSuccessDTO(obj.seller, SUM(obj.visited), SUM(obj.deals)) "
+			+ "FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSuccessDTO> successGroupedBySeller();
 }
